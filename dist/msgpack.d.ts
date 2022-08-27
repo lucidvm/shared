b8909306583861485de8bc08d72a6b28a0dc3a2c
dither <111248171+dithercat@users.noreply.github.com>
2022-08-26 22:11:47 -0400

    clean up dist, bump version

--- dist/msgpack.d.ts
+++
@@ -1,7 +0,0 @@
-export function serialize(data: any, options?: any): Uint8Array;
-export function deserialize(array: any, options?: any): any;
-declare namespace _default {
-    export { serialize };
-    export { deserialize };
-}
-export default _default;
